73cbca85202490b14ec556b07f49bf93a7d24c91
Izmayluv <91906602+Izmayluv@users.noreply.github.com>
2022-09-12 13:38:09 +0300

    Maket is done

--- app/src/main/java/com/example/figma/Third.java
+++ app/src/main/java/com/example/figma/Third.java
@@ -2,7 +2,9 @@ package com.example.figma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Third extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class Third extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third2);
 
+    }
+
+    public void BackScreen(View view) {
+        Intent intent = new Intent(this, Second.class);
+        startActivity(intent);
     }
 }
